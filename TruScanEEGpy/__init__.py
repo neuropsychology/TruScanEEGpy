b0b1c203b28abed3f803cda181613c43587d12e7
Dominique Makowski <dom.mak19@gmail.com>
2021-10-23 12:06:40 +0800

    v0.0.5 and fix channel names based on standard 10-5

--- TruScanEEGpy/__init__.py
+++ TruScanEEGpy/__init__.py
@@ -8,16 +8,15 @@
 import os
 
 __author__ = """TruScanEEGpy developpment team"""
-__email__ = 'dom.makowski@gmail.com'
-__version__ = '0.0.4'
+__email__ = "dom.makowski@gmail.com"
+__version__ = "0.0.5"
 __modulepath__ = os.path.split(__file__)[0]
 
 
-
+from .layout import *
 
 # =============================================================================
 # Imports
 # =============================================================================
 from .layouts import *
-from .layout import *
 from .montage import *
